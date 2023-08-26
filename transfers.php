734ce4a1f71074396c56027f36e1eb2548a0931b
Pius_dev <105377990+Pius-developer@users.noreply.github.com>
2023-08-26 22:29:11 +0100

    Update transfers.php

--- transfers.php
+++ transfers.php
@@ -3,10 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <!--========== BOX ICONS ==========-->
         <link href='npm/boxicons%402.0.5/css/boxicons.min.css' rel='stylesheet'>
-
         <!--========== CSS ==========-->
         <link rel="stylesheet" href="assets/css/styles.css">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">

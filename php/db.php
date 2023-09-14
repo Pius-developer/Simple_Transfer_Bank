d140460ac37572930576775931727191c6230a0a
Pius_dev <105377990+Pius-developer@users.noreply.github.com>
2023-09-14 05:32:39 +0100

    Update db.php

--- php/db.php
+++ php/db.php
@@ -5,13 +5,10 @@
    $database = "transbank";
    $databaseuser = "root";
    $password = "";
-
    $conn = mysqli_connect( $server, $databaseuser, $password, $database);
 
    if (!$conn) {
    	
    	mysqli_error();
    }
-
-
 ?>
